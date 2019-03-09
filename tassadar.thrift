f696d473e27a9e5f8b7f73fd2016d0207456ac9d
Shen, Fei <mathe.hacker@gmail.com>
2019-03-09 23:43:56 +0800

    update thrift: add cut_image API

--- tassadar.thrift
+++ tassadar.thrift
@@ -2,4 +2,5 @@ service TassadarServer {
     string version(),
     string get_ocr(1:binary image),
     string line_ocr(1:binary image),
+    list<binary> cut_image(1:binary image, 2:i8 cut_type),
 }
